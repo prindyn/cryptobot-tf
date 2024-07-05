971f7deaae7684b809ab62bc135058e1eaa511e5
Vasyl Prindyn <vprindyn@Vasyls-MBP.ht.home>
2024-07-05 11:56:33 -0500

    Create security group

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
     owners = ["979382823631"]
 }
 
-resource "aws_vpc" "dev" {
+resource "aws_vpc" "default_vpc" {
     cidr_block = "10.0.0.0/16"
 
     tags = {
@@ -23,7 +23,7 @@ resource "aws_vpc" "dev" {
 }
 
 resource "aws_subnet" "dev_subnet" {
-    vpc_id     = aws_vpc.dev.id
+    vpc_id     = aws_vpc.default_vpc.id
     cidr_block = "10.0.1.0/24"
 
     tags = {
@@ -36,7 +36,46 @@ resource "aws_instance" "web" {
     instance_type = var.instance_type
     subnet_id     = aws_subnet.dev_subnet.id
 
+    vpc_security_group_ids = [aws_security_group.web.id]
+
     tags = {
         Name = "CryptoBot"
     }
+}
+
+resource "aws_security_group" "web" {
+    name        = "web"
+    description = "Allow http and https in. Allow everything out."
+
+    vpc_id = aws_vpc.default_vpc.id
+}
+
+resource "aws_security_group_rule" "web_http_in" {
+    type        = "ingress"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    security_group_id = aws_security_group.web.id
+}
+
+resource "aws_security_group_rule" "web_https_in" {
+    type        = "ingress"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    security_group_id = aws_security_group.web.id
+}
+
+resource "aws_security_group_rule" "web_out" {
+    type        = "egress"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    security_group_id = aws_security_group.web.id
 }
